d7bb462834dae2346fc8b4670725ca3434d481f6
PhilipDet <philipdethlefsen251@gmail.com>
2024-11-12 13:17:57 +0100

    Song on html

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
-
 dotenv.config();
-console.log(process.env.SUPABASE_URL);
-console.log(process.env.SUPABASE_KEY);
+import { supabase } from "./config/supabase.js";
 
 const app = express();
 
@@ -27,6 +25,33 @@ app.get("/products", (req, res) => {
 
 app.get("/contact", (req, res) => {
     res.send("Du kan kontakte os her...");
+});
+
+app.get("/test", async (req, res) => {
+    let { data: songs, error } = await supabase
+        .from("songs")
+        .select(`*, artists(name)`);
+    if (error) {
+        return res.status(500).send(`Error: ${error.message}`);
+    }
+
+    if (songs && songs.length > 0) {
+        console.log(songs);
+
+        res.send(
+            `<p>Songs found:</p>
+            <ul>${songs
+                .map(
+                    (song) =>
+                        `<li><strong>Title:</strong> ${song.title} - <strong>Artist:</strong> ${song.artists.name}</li>`
+                )
+                .join("")}</ul>`
+        );
+
+        res.end();
+    } else {
+        res.send("<p>No songs found</p>");
+    }
 });
 
 app.listen(process.env.PORT, () => {
